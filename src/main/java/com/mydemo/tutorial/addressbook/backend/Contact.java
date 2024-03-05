5ccbad058cb8b9cb2badb2ab06a2cb756accf9e8
Parambulatoraws <parambulatoraws@gmail.com>
2024-03-05 11:52:17 -0600

    our first commit

--- src/main/java/com/mydemo/tutorial/addressbook/backend/Contact.java
+++ src/main/java/com/mydemo/tutorial/addressbook/backend/Contact.java
@@ -12,7 +12,7 @@ import java.util.Date;
  * in the database and can also be easily converted to different formats like JSON.
  */
 // Backend DTO class. This is just a typical Java backend implementation
-// class and nothing Vaadin specific.
+// class and nothing Vaadin specific. Good job here.
 public class Contact implements Serializable, Cloneable {
 
     private Long id;
